536032c5b184147b1c4cd6f008cac3ded3626198
Keyblade <135443223+Keybalde@users.noreply.github.com>
2023-09-06 11:05:15 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,10 +10,10 @@ export default function Index() {
       <img width="500" src="/img/bck.png" />
       </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
-          Astra Bot
+          Keyblade Bot
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-       How about moderating your own server with Astra bot? Create your own free world and create your own playlist, experience this pleasure with the best sounds. 
+       En iyi bdfd botlarından biri olan Keybalde BDFD sizin sayenizde büyüdü ve tik aldı. Hepinize teşşekürlerimizi iletiyoruz. 
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
