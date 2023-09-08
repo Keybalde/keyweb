137d10b9c47da58cc3ddd2349195f0d04143537e
Keyblade <135443223+Keybalde@users.noreply.github.com>
2023-09-08 08:55:26 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -90,7 +90,7 @@ Vote Keyblade BDFD</a>
                   <p className="text-3xl text-white font-semibold">Best Your Bot</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Astra, the best and most up-to-date bot?</p>
                 </div>
-                <img src="https://cdn.discordapp.com/attachments/737348411568685066/983094813115170866/unknown.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+                <img src="https://cdn.discordapp.com/attachments/1102100896726388807/1149583621020209162/Screenshot_20230908_085437.jpg" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
       
         </div>
