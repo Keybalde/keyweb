ff4f7185332726f6fc6e478d4219e13bfceaa32f
Keyblade <135443223+Keybalde@users.noreply.github.com>
2023-09-08 10:20:56 +0300

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -30,16 +30,16 @@ export default function Footer() {
                                         />
                                     </Menu.Button>
 <div className="py-1"></div>
-<a href="https://top.gg/bot/924325576095973426">
-  <img src={"https://top.gg/api/widget/servers/924325576095973426.svg"}/>
+<a href="https://top.gg/tr/bot/1096736371172982867">
+  <img src={"https://top.gg/api/widget/servers/1096736371172982867.svg"}/>
 </a>
 <div className="py-1"></div>
-<a href="https://top.gg/bot/924325576095973426">
-  <img src={"https://top.gg/api/widget/upvotes/924325576095973426.svg"}/>
+<a href="https://top.gg/tr/bot/1096736371172982867">
+  <img src={"https://top.gg/api/widget/upvotes/1096736371172982867.svg"}/>
 </a>
 <div className="py-1"></div>
-<a href="https://top.gg/bot/924325576095973426">
-  <img src={"https://top.gg/api/widget/owner/924325576095973426.svg"}/>
+<a href="https://top.gg/tr/bot/1096736371172982867">
+  <img src={"https://top.gg/api/widget/owner/1096736371172982867.svg"}/>
 </a>
                                 </div>
                             </Menu>
