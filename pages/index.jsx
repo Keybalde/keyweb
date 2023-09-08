b0fe60257a6e10ba3fd5940f23f7c25cc24b3e3f
Keyblade <135443223+Keybalde@users.noreply.github.com>
2023-09-08 08:58:35 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -87,8 +87,8 @@ Vote Keyblade BDFD</a>
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
                   <img src="https://cdn.discordapp.com/attachments/737348411568685066/983094813115170866/unknown.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
-                  <p className="text-3xl text-white font-semibold">Best Your Bot</p>
-                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Astra, the best and most up-to-date bot?</p>
+                  <p className="text-3xl text-white font-semibold">Botların en iyisi</p>
+                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">En aktip bdfd botu olan Keyblade BDFD artık daha güzel.</p>
                 </div>
                 <img src="https://cdn.discordapp.com/attachments/1102100896726388807/1149583621020209162/Screenshot_20230908_085437.jpg" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
